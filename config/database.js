cdbb7c8d54e86e66bbbd7b7dc30a2c99cd4861a4
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2022-12-11 22:13:29 -0300

    Alteração na config
    Alterado por conta da limitação do ElephantSQL

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'arjuna.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'emzvduth'),
-      user: env('DATABASE_USERNAME', 'emzvduth'),
-      password: env('DATABASE_PASSWORD', 'Aa1EGUurRjgQYwPvFMIqIYcehldEhysD'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "database"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", false),
+    },
+    pool: {
+      min: 0,
+      max: 5,
     },
   },
-  
 });
